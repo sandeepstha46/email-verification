9052a5874192579ecc11eb051df5a1928157df73
sandeepstha46 <sandeepstha64@gmail.com>
2022-12-05 12:40:46 +0545

    [Add] reCaptcha with validation

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -37,7 +37,12 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+            'g-recaptcha-response' => 'required|captcha'
+        ],
+        [
+            'g-recaptcha-response.required' => 'Please verify that you are not a robot.',
+        ]
+    );
 
         $user = User::create([
             'name' => $request->name,
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -45,6 +45,12 @@
                                 name="password_confirmation" required />
             </div>
 
+            <!-- Captcha -->
+            <div class="mt-4">
+                {!! NoCaptcha::renderJs() !!}
+                {!! NoCaptcha::display() !!}
+            </div>
+
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
